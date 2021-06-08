ebec77f5a67f305bfbda166cb634f799cf88f7c0
Travis Hoki <travishoki@T-Hoki-Mac.hsd1.ut.comcast.net>
2021-06-08 01:01:14 -0600

    comment out test again

--- src/6-full-line-coverage/jedi.test.js
+++ src/6-full-line-coverage/jedi.test.js
@@ -6,12 +6,14 @@ Task:
 - Add the tests for full line coverage
 */
 describe('getSaberColor', () => {
+  /*
   it('getSaberColor - isJedi undefined', () => {
     const result = getSaberColor();
     const expectedResult = undefined;
 
     expect(result).toEqual(expectedResult);
   });
+  */
 
   /*
   it('getSaberColor - isJedi true', () => {
