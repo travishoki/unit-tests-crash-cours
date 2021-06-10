12ffd33eab5888ffa4be4cafeb0846a2849d5296
Travis Hoki <travishoki@T-Hoki-Mac.hsd1.ut.comcast.net>
2021-06-10 02:43:24 -0600

    remove unneeded undefined value

--- src/3-component-render-test/ComponentA.test.js
+++ src/3-component-render-test/ComponentA.test.js
@@ -12,12 +12,12 @@ describe('ComponentA render', () => {
     };
     const wrapper = shallow(<ComponentA {...props} />);
 
-    expect(wrapper.isEmptyRender()).toBe(undefined);
+    expect(wrapper.isEmptyRender()).toBe();
   });
 
   it('ComponentA render - empty render', () => {
     const wrapper = shallow(<ComponentA />);
 
-    expect(wrapper.isEmptyRender()).toBe(undefined);
+    expect(wrapper.isEmptyRender()).toBe();
   });
 });
