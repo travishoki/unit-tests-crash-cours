b6a7e434d4536b0cff9a0a5301cf3cbfc7ac9509
Travis Hoki <travis.hoki@gmail.com>
2021-12-15 16:07:39 -0700

    useQueryCharacters test

---
+++ src/7-hook-test/useQueryCharacters.test.js
@@ -0,0 +1,11 @@
+import { renderHook } from "@testing-library/react-hooks";
+
+import useQueryCharacters from "./useQueryCharacters";
+
+describe("useQueryCharacters", () => {
+  it("useQueryCharacters", () => {
+    const result = renderHook(() => useQueryCharacters()).result.current;
+
+    expect(result).toEqual(null);
+  });
+});
