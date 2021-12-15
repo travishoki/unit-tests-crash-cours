96aa51c7c431fc1a6ddb067b09aff91f503f6ea0
Travis Hoki <travis.hoki@gmail.com>
2021-12-15 11:04:39 -0700

    add test for valid sum

--- src/2-pure-funciton-props/numbers.test.js
+++ src/2-pure-funciton-props/numbers.test.js
@@ -5,6 +5,13 @@ Task:
 - Throw error for undefined props
 */
 describe("numbers - sum", () => {
+  it("Should return the sum of two numbers", () => {
+    const num1 = 1;
+    const num2 = 1;
+
+    expect(sum(num1, num2)).toEqual(2);
+  });
+
   it("Should throw an error for undefined props", () => {
     expect(sum).toThrow("num1 prop undefined");
   });
