86bf62e5853e12e521a6c3d4f0a8277bde8a061a
Travis Hoki <travis.hoki@gmail.com>
2021-12-15 10:41:29 -0700

    cleanup

--- src/5-sinon-simulate-click/ComponentA.js
+++ src/5-sinon-simulate-click/ComponentA.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-export const ComponentA = ({
-  handleClick,
-  value,
-}) => (
-  <button
-    onClick={() => handleClick(value)}
-    className="button"
-  >Click Me!</button>
+export const ComponentA = ({ handleClick, value }) => (
+  <button onClick={() => handleClick(value)} className="button">
+    Click Me!
+  </button>
 );
 
 ComponentA.propTypes = {
