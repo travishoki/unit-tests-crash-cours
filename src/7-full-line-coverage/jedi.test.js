325110bee16abb5b7605fb5b4fe015113d2ea9f9
Travis Hoki <travis.hoki@gmail.com>
2022-02-24 22:49:20 -0700

    change comments

--- src/7-full-line-coverage/jedi.test.js
+++ src/7-full-line-coverage/jedi.test.js
@@ -6,21 +6,27 @@ Task:
 - Add the tests for full line coverage
 */
 describe("getSaberColor", () => {
-  // it('Should return undefined if isJedi prop is not a boolean', () => {
-  //   const result = getSaberColor();
-  //   const expectedResult = undefined;
-  //   expect(result).toEqual(expectedResult);
-  // });
-  // it('Should return COLORS.GREEN if isJedi prop is true', () => {
-  //   const isJedi = true;
-  //   const result = getSaberColor(isJedi);
-  //   const expectedResult = COLORS.GREEN;
-  //   expect(result).toEqual(expectedResult);
-  // });
-  // it('Should return COLORS.RED if isJedi prop is false', () => {
-  //   const isJedi = false;
-  //   const result = getSaberColor(isJedi);
-  //   const expectedResult = COLORS.RED;
-  //   expect(result).toEqual(expectedResult);
-  // });
+  /*
+  it("Should return undefined if isJedi prop is not a boolean", () => {
+    const result = getSaberColor();
+    const expectedResult = undefined;
+    expect(result).toEqual(expectedResult);
+  });
+  */
+  /*
+  it('Should return COLORS.GREEN if isJedi prop is true', () => {
+    const isJedi = true;
+    const result = getSaberColor(isJedi);
+    const expectedResult = COLORS.GREEN;
+    expect(result).toEqual(expectedResult);
+  });
+  */
+  /*
+  it('Should return COLORS.RED if isJedi prop is false', () => {
+    const isJedi = false;
+    const result = getSaberColor(isJedi);
+    const expectedResult = COLORS.RED;
+    expect(result).toEqual(expectedResult);
+  });
+  */
 });
